1ad6e690c7c8a594ae3aef32a71a7c2d8791f12e
Shuo Li <shuoli199909@outlook.com>
2023-04-27 13:32:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Shuo Li', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Msc of Biomedical Engineering\
   D-ITET\
-  ETH Zürich\', // 作者简介
+  ETH Zürich', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://shuoli199909.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
